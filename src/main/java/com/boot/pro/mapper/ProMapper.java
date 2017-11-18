ed236458c999c94a66a7ae468d066def7bc1fdbf
IamNotShady <zhou.xiaoxiao@outlook.com>
2017-11-18 22:30:41 +0800

    add summary of authority management

--- src/main/java/com/boot/pro/mapper/ProMapper.java
+++ src/main/java/com/boot/pro/mapper/ProMapper.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ProMapper extends BaseMapper<ProBean> {
 
-    @Cacheable(value = "basPro", key = "'getBasProList_'+#p0.getPro_name()+#p0.getPro_code()", unless="#result == null")
+    @Cacheable(value = "basPro", key = "'getBasProList_'+#p0.getPro_name()+#p0.getPro_code()", unless = "#result == null")
     List<ProBean> getBasProList(ProBean bean);
 
 }
