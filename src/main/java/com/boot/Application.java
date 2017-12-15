5964ddebc170933dd2c927ae573c5b4ad0b54b64
IamNotShady <zhou.xiaoxiao@outlook.com>
2017-12-16 02:01:26 +0800

    code format

--- src/main/java/com/boot/Application.java
+++ src/main/java/com/boot/Application.java
@@ -11,10 +11,10 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement// 启动注解事务管理，等同于xml配置方式的 <tx:annotation-driven />
 @ComponentScan(basePackages = {"com.boot"})
 @MapperScan("com.boot.*.mapper")
-public class SbwebApplication {
+public class Application {
 
     public static void main(String[] args) throws Exception {
         // 启动Spring Boot项目的唯一入口
-        new SpringApplication(SbwebApplication.class).run(args);
+        new SpringApplication(Application.class).run(args);
     }
 }
